3743bf885297edfb8958d0b6ac32fdbbf94e290d
Stefan Daniel <stefan.daniel99@gmx.at>
2018-12-06 12:45:12 +0100

    initial commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+import $ = require ('jquery');
+
+(async function() {
+    const pokelist = await $.get('https://pokeapi.co/api/v2/pokemon/');
+
+    let html = '';
+    for(const pokemon of pokelist.results) {
+        html += `<li>${pokemon.name}</li>`
+    }
+
+    $('#pokemons')[0].innerHTML = html;
+    return true;
+})();
